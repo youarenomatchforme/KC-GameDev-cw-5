5af9cd935a6c3804dce87d9a8bad17882015a902
youarenomatchforme <ghadoorah07@gmail.com>
2022-08-12 19:35:57 +0300

    balls and bricks and paddles 2

---
+++ rip fives/Assets/paddle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle : MonoBehaviour
+{
+    Rigidbody2D RB;
+    float playerInput;
+    public float speed; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        playerInput = Input.GetAxis("Horizontal"); 
+        RB.velocity = new Vector2(playerInput * speed, 0);
+    }
+}
